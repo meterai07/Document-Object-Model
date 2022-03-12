1c63cc500b9c74851c8496901101ad574c8fc6bc
Tengku rafi <t.mrr39@gmail.com>
2022-03-12 19:50:40 +0700

    commit

---
+++ dom image gallery/script.js
@@ -0,0 +1,24 @@
+const container = document.querySelector(".container");
+const jumbo = document.querySelector(".jumbo");
+const thumbs = document.querySelectorAll(".thumb");
+
+container.addEventListener("click", function (e) {
+    // cek click yang diclick thumb
+    if (e.target.className == "thumb") {
+        jumbo.src = e.target.src;
+        jumbo.classList.add("fade");
+        setTimeout(function () {
+            jumbo.classList.remove("fade")
+        }, 200);
+
+        thumbs.forEach(function (thumb) {
+            // if (thumb.classList.contains("active")) {
+            //     thumb.classList.remove("active");
+            // }
+            thumb.className = "thumb";
+        });
+
+        e.target.classList.add("active");
+    }
+});
+
